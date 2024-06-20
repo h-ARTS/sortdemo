5fd18f0d907a817f15454448203e725b5fcb8a14
Hanan Mufti <hi5hanan@yahoo.com>
2024-06-20 19:06:44 +0200

    init repo with files

---
+++ src/main/java/ch/fhnw/algd/sortalgs/HeapSort.java
@@ -0,0 +1,18 @@
+/*
+ * Created on 21.03.2014
+ */
+package ch.fhnw.algd.sortalgs;
+
+import ch.fhnw.algd.sortdemo.framework.SortAlg;
+import ch.fhnw.algd.sortdemo.framework.SortData;
+
+public class HeapSort implements SortAlg {
+	@Override
+	public void run(SortData data) { // [1, 2, 5, 9, 3, 6, 7, 19, 10, 4, 8, 13]
+
+	}
+
+	private void heapSort(SortData data, int start, int end) {
+
+	}
+}
