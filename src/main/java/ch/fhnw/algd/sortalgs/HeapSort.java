27d46d1fec8cd0ab223ee328e2809f50ae930147
Hanan Mufti <hi5hanan@yahoo.com>
2024-06-22 12:14:00 +0200

    add heapsort implementation

--- src/main/java/ch/fhnw/algd/sortalgs/HeapSort.java
+++ src/main/java/ch/fhnw/algd/sortalgs/HeapSort.java
@@ -8,11 +8,40 @@ import ch.fhnw.algd.sortdemo.framework.SortData;
 
 public class HeapSort implements SortAlg {
 	@Override
-	public void run(SortData data) { // [1, 2, 5, 9, 3, 6, 7, 19, 10, 4, 8, 13]
-
+	public void run(SortData data) { // [9, 1, 4, 5, 10, 6, 7, 19, 2, 3, 8, 13]
+		createFloydMaxHeap(data);
+		heapSort(data);
 	}
 
-	private void heapSort(SortData data, int start, int end) {
+	private void createFloydMaxHeap(SortData data) {
+		int size = data.size();
+		for (int i = size / 2 - 1; i >= 0; i--) { // i-- -> 1
+			siftDown(data, i, size); // 1, 6
+		}
+	}
 
+	private void heapSort(SortData data) {
+		int size = data.size();
+		for (int i = size - 1; i > 0; i--) {
+			data.swap(0, i);
+			siftDown(data,0, i);
+		}
+	}
+
+	private void siftDown(SortData data, int parent, int size) {
+		int child = indexOfLargerChild(data, parent, size); // 1, 6 -> 6, -> 5
+		while (child < size && data.less(parent, child)) { // 5 < 6 && 1 < 5
+			data.swap(parent, child);
+			parent = child; // 5
+			child = indexOfLargerChild(data, child, size); // 5, 6
+		}
+	}
+
+	private int indexOfLargerChild(SortData data, int parent, int size) {
+		int child = 2 * parent + 1; // 11
+		if (child + 1 < size && data.less(child, child + 1)) { // 4 < 6 && 4 < 5 true -> child = 5
+			++child;
+		}
+		return child;
 	}
 }
